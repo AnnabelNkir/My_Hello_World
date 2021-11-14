118ebf2dd0e3c3a8dbb0fadfe0272649f9b56400
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-14 17:56:19 +0300

    This commit installs some dependencies

--- manage.py
+++ manage.py
@@ -1,31 +1,32 @@
 from app import create_app, db
 from flask_script import Manager, Server
-
-from app.models import User
 from flask_migrate import Migrate, MigrateCommand
+from app.models import User, Post, Comment, PostLike
 
-# Creating app instance
-app = create_app('development')
-
+app = create_app("production")
 manager = Manager(app)
+manager.add_command("server", Server)
+
 migrate = Migrate(app, db)
-
-manager.add_command('server', Server)
-manager.add_command('db', MigrateCommand)
-
+manager.add_command("db",MigrateCommand)
 
 @manager.command
 def test():
-    """Run the unit tests."""
+    """
+    Run the unittests
+    """
     import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity = 2).run(tests)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
-
+    return dict(app = app,
+                db = db,
+                User = User, 
+                Post = Post,
+                Comment = Comment,
+                PostLike = PostLike)
 
 if __name__ == '__main__':
     manager.run()
