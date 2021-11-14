25a02ef9fc8f7f0fce2e8062fa276af269e1a87a
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-14 18:05:52 +0300

    This commit adds unnitests for the posts

---
+++ tests/test_post.py
@@ -0,0 +1,22 @@
+import unittest
+from app.models import Post, User, Comment
+
+class TestPost(unittest.TestCase):
+    
+    def setUp(self):
+        self.user_Annabel = User(first_name = "Annabel",
+                                last_name = "Micheni",
+                                username = "AnnabelNkir",
+                                password = "easy",
+                                email = "pitchybellah@mail.com")
+        self.new_post = Post(post_title = "Sample Title",
+                            post_content = "Hey you!",
+                            user_id = self.user_Annabel.id)
+        self.new_comment = Comment(comment = "Nice job",
+                                    post_id = self.new_post.id,
+                                    user_id = self.user_Annabel.id)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user_Collins, User))
+        self.assertTrue(isinstance(self.new_post, Post))
+        self.assertTrue(isinstance(self.new_comment, Comment))
