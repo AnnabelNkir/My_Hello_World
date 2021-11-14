a772e57175bfc8ff25fe3ae89a6ad62530a79c8c
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-14 16:23:54 +0300

    This commit adds authentification routes

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,35 +1,50 @@
-from .. import db
-from flask import render_template,redirect,url_for,flash,request
-from app.models import User
-from . import auth
-from flask import render_template,redirect,url_for, flash,request
+from flask import (render_template, redirect, url_for,
+                  flash, request)
 from flask_login import login_user, logout_user, login_required
-from .forms import RegistrationForm, LoginForm
+from . import auth
+from ..models import User
+from .forms import SignUpForm, LoginForm
 from .. import db
-from ..email import mail_message
+# from ..email import mail_message
 
+@auth.route("/signup", methods = ["GET", "POST"])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+    signup_form = SignUpForm()
+    if signup_form.validate_on_submit():
+        user = User(first_name = signup_form.first_name.data,
+                    last_name = signup_form.last_name.data,  
+                    username = signup_form.username.data, 
+                    email = signup_form.email.data,
+                    password = signup_form.password.data)
         db.session.add(user)
         db.session.commit()
-        flash('You have been successfully  signed up')
-        mail_message("Welcome to Pitches!!!","email/welcome_user",user.email,user=user)
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-    return render_template('auth/register.html',registration_form = form)
 
-@auth.route('/login',methods=['GET','POST'])
+        # mail_message("Welcome to 60 seconds",
+        #              "email/welcome", user.email, user = user)
+        return redirect(url_for("auth.login"))
+    title = "Sign Up to 60 Seconds"
+    return render_template("auth/signup.html", 
+                            signup_form = signup_form,
+                            title = title)
+
+@auth.route("/login", methods = ["GET", "POST"])
 def login():
     login_form = LoginForm()
     if login_form.validate_on_submit():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
-            login_user(user,login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get("next") or url_for("main.index"))
 
-        flash('Invalid username or Password')
+        flash("Invalid Username or Password")
+    
+    title = "Login to 60 Seconds"
+    return render_template("auth/login.html",
+                            login_form = login_form,
+                            title = title)
 
-    title = "SignIn | LetsBlog"
-    return render_template('auth/login.html',login_form = login_form,title=title)
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
