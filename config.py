e4af1fda31bd215130d216010239816d04d099b9
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-16 08:31:20 +0300

    Deployment

--- config.py
+++ config.py
@@ -21,11 +21,6 @@ class Config:
     def init_app(app):
         pass
 
-class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
-    pass
 
 class TestConfig(Config):
     pass
@@ -35,7 +30,10 @@ class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    
     pass
+
+
 
 
 class DevConfig(Config):
