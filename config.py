4b63da7d7195a3ee0cba4a0b8098efcf81f49bdb
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-16 05:32:23 +0300

    This commit deploys the appp to heroku

--- config.py
+++ config.py
@@ -22,15 +22,12 @@ class Config:
         pass
 
 class ProdConfig(Config):
-   
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     pass
 
 class TestConfig(Config):
-    pass
-
-
-class ProdConfig(Config):
-   
     pass
 
 
