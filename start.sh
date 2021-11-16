b05ed8bd46283f2ff6075f9b9d379e4d7f7165d3
AnnabelNkir <nkiroteannabel@gmail.com>
2021-11-16 09:01:26 +0300

    Updates config.py

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY=Bella123
+export SECRET_KEY='e739276f65406299c601d88f4f5a8164'
 export MAIL_USERNAME=pitchybellah@gmail.com
 export MAIL_PASSWORD=pitchybellah1!
 python3.8 manage.py server
